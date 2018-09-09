c3bd415e0aac7d0408133985f95e6352a7d00c1c
eboycai <gary.cai@ericsson.com>
2018-09-09 23:53:15 +0900

    Adjusting Our Models With Foreign Key 20180909

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render,redirect
-from lists.models import Item
+from django.shortcuts import render, redirect
+from lists.models import Item, List
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
@@ -9,20 +9,17 @@ from django.http import HttpResponse
 def home_page(request):
     # return HttpResponse('<html><title>To-Do lists</title></html>')
 
-    if request.method == 'POST':
-        # new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/lists/the-only-list-in-the-world/')
-    # else:
-    #     new_item_text = ''
-    #     item = Item()
-    # item.text = request.POST.get('item_text', '')
-    # item.save()
     return render(request, 'home.html')
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST.get('item_text', ''), list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
 
 
