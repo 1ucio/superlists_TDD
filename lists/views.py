bfec321309dd02fe5be4ed8c60b1f70468b4855c
eboycai <gary.cai@ericsson.com>
2018-09-09 19:02:03 +0900

    New URL, View And Template To Display Lists 20180909

--- lists/views.py
+++ lists/views.py
@@ -12,14 +12,17 @@ def home_page(request):
     if request.method == 'POST':
         # new_item_text = request.POST.get('item_text', '')
         Item.objects.create(text=request.POST.get('item_text', ''))
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     # else:
     #     new_item_text = ''
     #     item = Item()
     # item.text = request.POST.get('item_text', '')
     # item.save()
+    return render(request, 'home.html')
 
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
 
 
